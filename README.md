5ca6796e3e24305aeb2ed340604a1544a2d9c175
zjleon <zjleon2010@gmail.com>
2015-10-27 02:41:58 +0800

    first

---
+++ README.md
@@ -0,0 +1,29 @@
+fis-parser-jade-to-html
+===============
+
+A parser for fis to compile jade file.
+Output html
+
+## 功能
+
+ - complie jade template to html file
+ - Automatically resolve the path of resource include by 'extend', 'include' commands
+
+## 用法
+
+### 安装插件
+
+    $ npm install fis-parser-jade --save-dev
+
+### 启用插件
+
+```javascript
+fis.match('test.jade', {
+	parser: fis.plugin('jade-to-html', {
+		pretty: true,
+		// variables you want to give to the template complie function, JSON Format.
+		data: headerVars
+	}),
+	rExt: 'html'
+})
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var fs = require( 'fs' );
+var path = require( 'path' );
+var jade = require( 'jade' );
+var basedir = fis.project.getProjectPath();
+
+module.exports = function( content, file, conf ) {
+  var data = {};
+  if ( conf.data || file.extras.jade ) {
+    data = ( file.extras.jade && file.extras.jade.data ) || conf.data;
+    if ( data instanceof Function ) {
+      data = data( file );
+    }
+  }
+  if ( conf.filters ) {
+    Object.keys( conf.filters ).forEach( function( filter ) {
+      jade.filters[ filter ] = conf.filters[ filter ].bind( file );
+    } );
+  }
+  conf.basedir = conf.basedir || basedir;
+  var template = jade.compile( content, conf );
+
+  // fis.log.info(file);
+  var resourceList = [];
+  var jadeList = template.dependencies;
+  jadeList.forEach( function( filePath ) {
+    // get current file path
+    var currentPath = path.dirname( filePath );
+    // fis.log.info(currentPath);
+    var file = fis.file( filePath );
+    // fis.log.info('each loop');
+    var content = file.getContent();
+    // TODO: convert sass file path
+    var reg = /(link|script|img)\(.*(src|href)=['"](.+?)['"].*\)/g;
+    var tmp1;
+    // search the whole content to get all resources
+    do {
+      tmp1 = reg.exec( content );
+      if ( !tmp1 ) {
+        break;
+      }
+      resourceList.push( {
+          fullMatch: tmp1[ 0 ],
+          type: tmp1[ 1 ],
+          relativePath: tmp1[ 3 ],
+          absolutePath: path.resolve( currentPath, tmp1[ 3 ] )
+        } )
+        // fis.log.info(resourceInFile);
+    } while ( tmp1.index < content.length - 1 )
+  } );
+  // fis.log.info(resourceList);
+
+  // file.useCache = false;
+  // fis.compile.process(file);
+  // fis.log.info(file);
+
+  template.dependencies.forEach( function( dep ) {
+    file.cache.addDeps( dep );
+  } );
+
+  var exportContent = template( data );
+  // replace resource path base on this template location, after template complied
+  var root = file.realpath.replace( file.subpath, '' );
+  // fis.log.info(root);
+  resourceList.forEach( function( tmp ) {
+    var pathBaseOnRoot = '';
+    if ( fs.statSync( tmp.absolutePath ).isFile() ) {
+      // file.addLink(tmp.absolutePath);
+      pathBaseOnRoot = tmp.absolutePath.replace( root, '' );
+      // fis.log.info(tmp.relativePath);
+      // fis.log.info(pathBaseOnRoot);
+      exportContent = exportContent.replace( tmp.relativePath, pathBaseOnRoot );
+    }
+  } );
+  // fis.log.info(exportContent);
+  return exportContent;
+};
